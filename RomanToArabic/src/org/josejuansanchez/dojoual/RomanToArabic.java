52b95668e8d45dc701d4d5c109c815a7736b7edc
José Juan Sánchez Hernández <josejuansanchez@Las-Salinas.local>
2014-05-14 00:41:58 +0200

    Changes in RomanToArabic

--- RomanToArabic/src/org/josejuansanchez/dojoual/RomanToArabic.java
+++ RomanToArabic/src/org/josejuansanchez/dojoual/RomanToArabic.java
@@ -1,8 +1,26 @@
 package org.josejuansanchez.dojoual;
 
+import java.util.HashMap;
+
 public class RomanToArabic {
 
-	public static int roman_to_arabic(String roman_number) {
+	private HashMap<String, Integer> roman_symbols;		
+	
+	public RomanToArabic() {
+		roman_symbols = new HashMap<String, Integer>();
+		roman_symbols.put("I",1);
+		roman_symbols.put("V",5);
+		roman_symbols.put("X",10);
+		roman_symbols.put("L",50);		
+		roman_symbols.put("C",100);
+		roman_symbols.put("D",500);
+		roman_symbols.put("M",1000);		
+	}
+	
+	public int roman_to_arabic(String roman_number) {
+		
+		roman_number = roman_number.toUpperCase();		
+		
 		int arabic_number = 0;
 		int length = roman_number.length();
 		for(int i=0; i < length - 1; i++) {			
@@ -15,42 +33,11 @@ public class RomanToArabic {
 				arabic_number = arabic_number + current_value;				
 			}						
 		}
-		arabic_number = arabic_number + get_value_of_roman_symbol(roman_number.charAt(length-1));				
+		arabic_number = arabic_number + get_value_of_roman_symbol(roman_number.charAt(length-1));
 		return arabic_number;
 	}
 
-	private static int get_value_of_roman_symbol(char symbol) {
-		int value = -1;
-		switch(symbol) {
-			case 'i': 
-				value = 1;
-				break;
-			
-			case 'v': 
-				value = 5;
-				break;
-			
-			case 'x': 
-				value = 10;
-				break;
-				
-			case 'l':
-				value = 50;
-				break;
-				
-			case 'c':
-				value = 100;
-				break;
-				
-			case 'd':
-				value = 500;
-				break;
-				
-			case 'm':
-				value = 1000;
-				break;
-		
-		}
-		return value;
+	private Integer get_value_of_roman_symbol(char symbol) {				
+		return roman_symbols.get(Character.toString(symbol));
 	}
 }
--- RomanToArabic/src/org/josejuansanchez/dojoual/RomanToArabicTest.java
+++ RomanToArabic/src/org/josejuansanchez/dojoual/RomanToArabicTest.java
@@ -6,101 +6,101 @@ import org.junit.Test;
 
 public class RomanToArabicTest {
 
+	RomanToArabic converter = new RomanToArabic();
+	
 	@Test
-	public void when_i_then_1() {
-		int result = RomanToArabic.roman_to_arabic("i");
+	public void when_i_then_1() {		
+		int result = converter.roman_to_arabic("i");
 		assertTrue(result == 1);
 	}
 
 	@Test
-	public void when_v_then_5() {
-		int result = RomanToArabic.roman_to_arabic("v");
+	public void when_v_then_5() {		
+		int result = converter.roman_to_arabic("v");
 		assertTrue(result == 5);
 	}
 
 	@Test
 	public void when_x_then_10() {
-		int result = RomanToArabic.roman_to_arabic("x");
+		int result = converter.roman_to_arabic("x");
 		assertTrue(result == 10);
 	}	
 
 	@Test
 	public void when_l_then_50() {
-		int result = RomanToArabic.roman_to_arabic("l");
+		int result = converter.roman_to_arabic("l");
 		assertTrue(result == 50);
 	}	
 		
 	@Test
 	public void when_c_then_100() {
-		int result = RomanToArabic.roman_to_arabic("c");
+		int result = converter.roman_to_arabic("c");
 		assertTrue(result == 100);
 	}	
 	
 	@Test
 	public void when_d_then_500() {
-		int result = RomanToArabic.roman_to_arabic("d");
+		int result = converter.roman_to_arabic("d");
 		assertTrue(result == 500);
 	}	
 
 	@Test
 	public void when_m_then_1000() {
-		int result = RomanToArabic.roman_to_arabic("m");
+		int result = converter.roman_to_arabic("m");
 		assertTrue(result == 1000);
 	}
 
 	@Test
 	public void when_ii_then_2() {
-		int result = RomanToArabic.roman_to_arabic("ii");
+		int result = converter.roman_to_arabic("ii");
 		assertTrue(result == 2);
 	}	
 	
 	@Test
 	public void when_iii_then_3() {
-		int result = RomanToArabic.roman_to_arabic("iii");
+		int result = converter.roman_to_arabic("iii");
 		assertTrue(result == 3);
 	}
 	
 	@Test
 	public void when_iv_then_4() {
-		int result = RomanToArabic.roman_to_arabic("iv");
+		int result = converter.roman_to_arabic("iv");
 		assertTrue(result == 4);
 	}		
 	
 	@Test
 	public void when_vi_then_6() {
-		int result = RomanToArabic.roman_to_arabic("vi");
+		int result = converter.roman_to_arabic("vi");
 		assertTrue(result == 6);
 	}
 	
 	@Test
 	public void when_lxxx_then_80() {
-		int result = RomanToArabic.roman_to_arabic("lxxx");
+		int result = converter.roman_to_arabic("lxxx");
 		assertTrue(result == 80);
 	}
 
 	@Test
 	public void when_mxxc_then_1080() {
-		int result = RomanToArabic.roman_to_arabic("mlxxx");
+		int result = converter.roman_to_arabic("mlxxx");
 		assertTrue(result == 1080);
 	}
 	
 	@Test
 	public void when_mmxxc_then_2080() {
-		int result = RomanToArabic.roman_to_arabic("mmlxxx");
+		int result = converter.roman_to_arabic("mmlxxx");
 		assertTrue(result == 2080);
 	}
 
 	@Test
 	public void when_cxl_then_140() {
-		int result = RomanToArabic.roman_to_arabic("cxl");
+		int result = converter.roman_to_arabic("cxl");
 		assertTrue(result == 140);
 	}
-
 	
 	@Test
 	public void when_mmmxcviii_then_3098() {
-		int result = RomanToArabic.roman_to_arabic("mmmxcviii");
+		int result = converter.roman_to_arabic("mmmxcviii");
 		assertTrue(result == 3098);
-	}
-	
+	}	
 }
